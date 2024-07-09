cd9f9955d29af5331ef2391cba66dff95d21eab9
Vic <newwave@gmail.com>
2024-07-09 17:01:36 -0400

    new

--- services/users.js
+++ services/users.js
@@ -28,7 +28,7 @@ async function registerNewUsers(user) {``
       `INSERT INTO users 
       (name, username, email, email_verified_at, password, saldo, remember_token) 
       VALUES 
-      ('${user.name}', '${user.username}', '${user.email}', '${user.email_verified_at}', '${user.password}', 0, '${user.remember_token}}')`
+      ('${user.name}', '${user.username}', '${user.email}', '${new Date()}', '${user.password}', 0, '${user.remember_token}}')`
     );
     let message = 0
     if (result.affectedRows) {
