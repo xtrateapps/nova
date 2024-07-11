362477d0605d4479604da5c69a10fe41b088c970
Vic <newwave@gmail.com>
2024-07-10 22:24:15 -0400

    directtran

--- services/transactions.js
+++ services/transactions.js
@@ -45,9 +45,17 @@ async function registerNewTransaction(transaction) {
 async function sendDirectFundsFromOneUserToAnother(transaction) {
   // return transaction;
   let result = await db.query(
-    `SELECT * FROM users WHERE username = '${transaction.username}' LIMIT 1` 
+    `SELECT sald0 FROM users WHERE username = '${transaction.username}' LIMIT 1` 
   );
-  return result;   
+  let saldoReceptor = result[0].saldo
+  let result2 = await db.query(
+    `SELECT sald0 FROM users WHERE username = '${transaction.username}' LIMIT 1` 
+  );
+  let saldoEmisor = result[0].saldo
+  return {
+    saldoReceptor,
+    saldoEmisor
+  }
   // let message = 'Datos no encontrados';
   console.log(result);
   console.log(message);
