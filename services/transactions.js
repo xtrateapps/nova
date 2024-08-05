1be47c0c056bad0b757e59c47dddd9cc11aa6def
Vic <newwave@gmail.com>
2024-08-04 21:40:48 -0400

    transactions xd

--- services/transactions.js
+++ services/transactions.js
@@ -55,7 +55,7 @@ async function sendDirectFundsFromOneUserToAnother(transaction) {
     }
   } else {
     let transactionUsername = transaction.username
-      let transactionDestiny = transaction.destiny
+    let transactionDestiny = transaction.destiny
     let saldoCheck = await db.query(
       `SELECT saldo FROM users WHERE username = '${transactionUsername}' LIMIT 1` 
     ); 
@@ -207,7 +207,7 @@ async function getAllTransactionsrelatedToUser(user) {
   console.log(user.bank);
   console.log("----------------------------- todas las transacciones -------------------------------sss--");
   // const result = await db.query(`SELECT * FROM transactions WHERE bank = '${user.bank}' AND account_number = '${user.account_number}'`)
-  const result = await db.query(`SELECT * FROM transactions WHERE bank = '${user.bank}' AND user_logged = '${user.bank}'`);
+  const result = await db.query(`SELECT * FROM transactions WHERE user_logged = '${user.username}'`);
   // let message = 'Error in getting all transaction related to user';
   console.log(result);
   let message = "exito";
